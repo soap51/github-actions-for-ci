22d522b0e7797650ce213ec1532296b9d450e46b
Poramet Thawinkarn <35328191+soap51@users.noreply.github.com>
2021-09-11 22:26:38 +0700

    Fixed unused

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,9 +1,9 @@
 const Game = require('../src/game').default
-const fs = require('fs')
+// const fs = require('fs')
 
 describe('App', () => {
   it('Contains the compiled JavaScript', async () => {
-    const data = fs.readFileSync('./public/main.js', 'utf8')
+  //  const data = fs.readFileSync('./public/main.js', 'utf8')
 //     expect(data).toMatchSnapshot()
   })
 })
