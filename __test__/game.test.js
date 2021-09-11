6afb7afa6de7bd30c8065092393061d3bf2cea0e
Poramet Thawinkarn <35328191+soap51@users.noreply.github.com>
2021-09-11 22:30:12 +0700

    Fixed CI

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,10 +1,10 @@
 const Game = require('../src/game').default
-// const fs = require('fs')
+const fs = require('fs')
 
 describe('App', () => {
   it('Contains the compiled JavaScript', async () => {
-  //  const data = fs.readFileSync('./public/main.js', 'utf8')
-//     expect(data).toMatchSnapshot()
+   const data = fs.readFileSync('./public/main.js', 'utf8')
+    expect(data).toMatchSnapshot()
   })
 })
 
